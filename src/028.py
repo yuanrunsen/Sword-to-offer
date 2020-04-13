cebb5a0897f0541e51705a1c50331bbe83c9dbc4
yuanrunsen <yuanrunsen@unitedstack.com>
2020-04-13 15:53:13 +0800

    modify 028, add queue.
    Signed-off-by: yuanrunsen <yuanrunsen@unitedstack.com>

--- src/028.py
+++ src/028.py
@@ -7,26 +7,67 @@ class Stack(object):
     def __init__(self):
         self.storage = list()
         self.storage_sup = list()
-        self._min = None
 
     def push(self, value):
         self.storage.append(value)
-        if not self._min:
-            self._min = value
+        if not self.storage_sup:
             self.storage_sup.append(value)
-        elif value <= self._min:
-            self._min = value
+        elif value <= self.storage_sup[-1]:
             self.storage_sup.append(value)
 
     def pop(self):
         value = self.storage.pop()
-        if value == self._min:
+        if value == self.storage_sup[-1]:
             self.storage_sup.pop()
-            if self.storage_sup:
-                self._min = self.storage_sup[-1]
-            else:
-                self._min = None
         return value
 
     def min(self):
-        return self._min
+        # 栈顶元素永远是最小值
+        if not self.storage_sup:
+            return None
+        return self.storage_sup[-1]
+
+
+# 面试题30(自定义)：包含min函数的队列
+# 题目：定义队列的数据结构，请在该类型中实现一个能够得到队列的最小元素的min
+# 函数。在该栈中，调用min，的时间复杂度都是O(1)。
+import queue
+
+
+class Queue(object):
+    def __init__(self):
+        self.storage = queue.Queue()
+        self.storage_sup = list()
+
+    def put(self, value):
+        self.storage.put(value)
+        if not self.storage_sup:
+            self.storage_sup.append(value)
+        else:
+            if value >= self.storage_sup[-1]:
+                self.storage_sup.append(value)
+            else:
+                # 按顺序插入 并且删除其后的元素
+                # 例如 3 4 5 6 7 5的队列， 其最小值不可能比5还大
+                # 那么 这里的辅助数组只用保存3 4 5即可
+                # 下面的for循环就是做了这个事情
+                for i in range(len(self.storage_sup)):
+                    if self.storage_sup[i] > value:
+                        self.storage_sup = self.storage_sup[:i+1]
+                        self.storage_sup[-1] = value
+                        break
+                    if i == len(self.storage_sup)-1:
+                        self.storage_sup.append(value)
+
+    def get(self):
+        value = self.storage.get()
+        if value == self.storage_sup[0]:
+            self.storage_sup = self.storage_sup[1:]
+        return value
+
+    def min(self):
+        # 队首元素永远是最小值
+        if not self.storage_sup:
+            return None
+        return self.storage_sup[0]
+
